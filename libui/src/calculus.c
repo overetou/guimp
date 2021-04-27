9bbe1bcd7548cd1d94f2eefd6f57eeeb2a10de28
overetou <overetou@student.42.fr>
2021-04-27 09:40:32 +0200

    The positionning is set right.

--- libui/src/calculus.c
+++ libui/src/calculus.c
@@ -13,8 +13,10 @@ void ui_calculate_win_content_actual_size(t_ui_win *win)
 
 void ui_infer_elem_actual_size(t_ui_elem *e)
 {
-	e->actual_sizes.x = e->parent->actual_sizes.x * e->proportions.x / 100;
-	e->actual_sizes.y = e->parent->actual_sizes.y * e->proportions.y / 100;
+	e->actual_sizes.x = e->parent->actual_sizes.x + e->parent->actual_sizes.w
+			* e->proportions.x / 100;
+	e->actual_sizes.y = e->parent->actual_sizes.y + e->parent->actual_sizes.h
+			* e->proportions.y / 100;
 	e->actual_sizes.w = e->parent->actual_sizes.w * e->proportions.w / 100;
 	e->actual_sizes.h = e->parent->actual_sizes.h * e->proportions.h / 100;
 }
