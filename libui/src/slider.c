207c674f4914e664aecb64eb5b447842a5417091
man <maur>
2021-05-20 13:55:14 +0200

    Added slider file with temporary prototypes.

---
+++ libui/src/slider.c
@@ -0,0 +1,21 @@
+#include "ui.h"
+
+void	ui_free_slider_store(void *to_free)
+{
+
+}
+
+void	ui_display_slider(t_ui_elem *e)
+{
+
+}
+
+void	ui_slider_clicked(t_ui_elem *e, SDL_MouseButtonEvent *ev)
+{
+
+}
+
+t_ui_elem	*ui_create_slider(t_ui_elem *parent, int x, int y, int w)
+{
+
+}
