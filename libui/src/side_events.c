418ce15f1b8847746eb52f3a47804cfe858fd7ae
man <maur>
2021-07-08 17:11:22 +0200

    ui_remove_side_event func done.

--- libui/src/side_events.c
+++ libui/src/side_events.c
@@ -1,15 +1,16 @@
 #include "ui.h"
 
-void	ui_add_side_event(t_ui_win *win, void (*event_handler_func)(void*, SDL_Event*), void *store)
+t_ui_side_event_block	*ui_add_side_event(t_ui_win *win, void (*event_handler_func)(void*, SDL_Event*), void *store)
 {
 	t_ui_side_event_block	*new_bloc = ui_secure_malloc(sizeof(t_ui_side_event_block));
 
 	new_bloc->event_handler_func = event_handler_func;
 	new_bloc->store = store;
 	new_bloc->next = win->side_events;
+	return new_bloc;
 }
 
-void	ui_add_exclusive_side_event(t_ui_win *win, void (*event_handler_func)(void*, SDL_Event*), void *store)
+t_ui_side_event_block	*ui_add_exclusive_side_event(t_ui_win *win, void (*event_handler_func)(void*, SDL_Event*), void *store)
 {
 	t_ui_side_event_block	*traveler = win->side_events;
 
@@ -18,8 +19,24 @@ void	ui_add_exclusive_side_event(t_ui_win *win, void (*event_handler_func)(void*
 		if (traveler->event_handler_func == event_handler_func)
 		{
 			traveler->store = store;
-			return;
+			return traveler;
 		}
 	}
-	ui_add_exclusive_side_event(win, event_handler_func, store);
+	return ui_add_exclusive_side_event(win, event_handler_func, store);
+}
+
+void	ui_remove_side_event(win, t_ui_side_event_block *to_remove)
+{
+	t_ui_side_event_block	*prev;
+
+	if (win->side_events == to_remove)
+		win->side_events = to_remove->next;
+	else
+	{
+		prev = win->side_events;
+		while (prev->next != to_remove)
+			prev = prev->next;
+		prev->next = to_remove->next;
+	}
+	free(to_remove);
 }
